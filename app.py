92996e22519ce65272054e160116d7fd26c7b4da
cristian toro <tcristian984@gmail.com>
2026-03-31 10:55:14 -0600

    Inicio proyecto TrueSnapp - Semana 1

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TrueSnapp",
+    page_icon="📸",
+    layout="centered"
+)
+
+st.title("📸 TrueSnapp")
+st.subheader("Fotografías profesionales para tu alojamiento")
+st.write("Bienvenido al prototipo de TrueSnapp")
